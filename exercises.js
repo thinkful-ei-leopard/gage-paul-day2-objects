ca4ec7998188af7c3b64db08c0e057a7d011ffbe
wavinginspace <paulbaisley@gmail.com>
2020-01-07 15:54:38 -0500

    exercise 2

--- exercises.js
+++ exercises.js
@@ -1,9 +1,27 @@
-let loaf = {
-    flour: 300,
-    water: 210,
-    hydration() {
-        return this.water / this.flour * 100
-    }
-}
+'use strict';
 
-console.log(loaf.hydration());
+// * Object initializers and methods
+
+// let loaf = {
+//     flour: 300,
+//     water: 210,
+//     hydration() {
+//         return this.water / this.flour * 100
+//     }
+// }
+
+// console.log(loaf.hydration());
+
+// * ITERATING OVER AN OBJECT'S PROPERTIES
+
+let myObject = {
+  foo: 'prop1',
+  bar: 'prop2',
+  fum: 'prop3',
+  quux: 'prop4',
+  spam: 'prop5'
+};
+
+for(const key in myObject) {
+  console.log(`${key}: ${myObject[key]}`);
+}
