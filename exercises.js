f4a051aceeb4bb8278b465a7792b0b098fac6ce3
wavinginspace <paulbaisley@gmail.com>
2020-01-07 19:13:42 -0500

    cracking the code

--- exercises.js
+++ exercises.js
@@ -34,29 +34,56 @@
 
 // * ARRAYS OF OBJECTS & PROPERTIES THAT ARENT THERE
 
-let benObject = {
-  name: 'Ben',
-  jobTitle: 'teacher',
-};
+// let benObject = {
+//   name: 'Ben',
+//   jobTitle: 'teacher',
+// };
 
-let leeObject = {
-  name: 'Lee',
-  jobTitle: 'producer',
-  boss: false
-};
+// let leeObject = {
+//   name: 'Lee',
+//   jobTitle: 'producer',
+//   boss: false
+// };
 
-let nickObject = {
-  name: 'Nick',
-  jobTitle: 'ranger',
-  boss: false
-};
+// let nickObject = {
+//   name: 'Nick',
+//   jobTitle: 'ranger',
+//   boss: false
+// };
 
-let peopleArray = [benObject, leeObject, nickObject];
+// let peopleArray = [benObject, leeObject, nickObject];
 
-peopleArray.forEach(person => {if (person.boss === false) {
-    console.log(`${person.jobTitle} ${person.name} reports to ${benObject.name}`);
-} else {
-    console.log(`${person.jobTitle} ${person.name} reports to nobody`);
-    }
+// peopleArray.forEach(person => {if (person.boss === false) {
+//     console.log(`${person.jobTitle} ${person.name} reports to ${benObject.name}`);
+// } else {
+//     console.log(`${person.jobTitle} ${person.name} reports to nobody`);
+//     }
+// }
+// );
+
+// * CRACKING THE CODE !!!!!!!!!!!!111111
+
+function decode(word) {
+  let wordObject = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4
+  };
+  if (word[0] !== 'a' && word[0] !== 'b' && word[0] !== 'c' && word[0] !== 'd') {
+    return ' ';
+  } else {
+    return word[wordObject[word[0]]];
+  }
+} 
+
+function decodeWords(sentence) {
+  let splitSentence = sentence.split(' ');
+  let newArray = [];
+  splitSentence.forEach(word => newArray.push(decode(word)));
+  let finalArray = newArray.join('');
+  return finalArray;
 }
-);
+
+let test = decodeWords('craft block argon meter bells brown croon droop');
+console.log(test);
